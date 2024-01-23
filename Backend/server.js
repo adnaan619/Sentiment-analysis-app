1374dc70aca09698e8cf5dce4d44b44fb5949aa4
adnaan619 <86790106+adnaan619@users.noreply.github.com>
2024-01-23 17:46:56 +0530

    Express server setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,22 @@
+require('dotenv').config();
+
 const express = require('express');
 
 //express app
 const app = express();
 
-//listen for requests
-app.listen(4000, () => {
-    console.log('listening on port 4000')
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+//routes
+app.get('/', (req, res) => {
+    res.json({msg: "Welcome to the app"});
 });
+
+//listen for requests
+app.listen(process.env.PORT, () => {
+    console.log('listening on port ', process.env.PORT);
+});
+
