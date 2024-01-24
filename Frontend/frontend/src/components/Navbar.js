f122f8b1033b67fc3b78839af40d4b4bed24d581
adnaan619 <86790106+adnaan619@users.noreply.github.com>
2024-01-24 23:49:30 +0530

    FE footer and header component designs

--- Frontend/frontend/src/components/Navbar.js
+++ Frontend/frontend/src/components/Navbar.js
@@ -1,18 +1,83 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Typography, Container, Grid, Button } from "@mui/material";
 
 function Navbar() {
   return (
-      <header>
-          <div className='container'>
-              <Link to="/">
-                  <h1>
-                      Workout Buddy
-                  </h1>
-              </Link>
-          </div>
-    </header>
-  )
+    <Container
+      disableGutters
+      maxWidth={false}
+      style={{ display: "flex", backgroundColor: "#4361ee", height: "100px" }}
+    >
+      {/* <Link to="/"> */}
+      <Grid
+        container
+        xs={12}
+        style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
+      >
+        <Grid
+          xs={8}
+          style={{
+            display: "flex",
+            justifyContent: "flex-start",
+          }}
+        >
+          <Typography
+            variant="h2"
+            fontWeight="bold"
+            style={{
+              color: "#ffffff",
+              fontFamily: "Poppins",
+              fontSize: "40px",
+              marginLeft: "120px",
+            }}
+          >
+            SentimentX
+          </Typography>
+        </Grid>
+        {/* </Link> */}
+        <Grid
+          xs={4}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Button
+            sx={{
+              width: "100px",
+              height: "30px",
+              borderRadius: "15px",
+              backgroundColor: "#2b35af",
+              color: "#ffffff",
+              marginRight: "10px",
+              "&:hover": {
+                backgroundColor: "#12086F",
+              },
+            }}
+          >
+            login
+          </Button>
+          <Button
+            sx={{
+              width: "100px",
+              height: "30px",
+              borderRadius: "15px",
+              backgroundColor: "#2b35af",
+              color: "#ffffff",
+              marginLeft: "5x",
+              "&:hover": {
+                backgroundColor: "#12086F",
+              },
+              marginRight: "20px",
+            }}
+          >
+            Signup
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default Navbar;
