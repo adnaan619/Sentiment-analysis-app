fd26665179f874e7586e201ae9068b12eb8e698e
adnaan619 <86790106+adnaan619@users.noreply.github.com>
2024-01-24 17:06:56 +0530

    Several changes made to deleting context

--- Frontend/frontend/src/index.js
+++ Frontend/frontend/src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { WorkoutsContextProvider } from './context/WorkoutsContext';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
+  <WorkoutsContextProvider>
     <App />
+  </WorkoutsContextProvider>
 );
